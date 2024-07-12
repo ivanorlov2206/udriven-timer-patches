3f51a2400697f13f528aca0d40810cba74740bf9
Ivan Orlov <ivan.orlov0322@gmail.com>
2024-07-12 23:23:51 +0100

    Refactor test.c and add more comments

--- test-app/test.c
+++ test-app/test.c
@@ -6,9 +6,14 @@
 #include <pthread.h>
 #include <sound/asound.h>
 
+/*
+ * Userspace timer definition. We have to provide the desired rate and period size (in frames)
+ * to convince snd-aloop that our timer works correctly and has right HW parameters
+ */
 static struct snd_userspace_timer timer = {
 	.rate = 8000,
 	.period = 4410,
+	/* id can be set to anything here, it is going to be overwritten by timer creation ioctl */
 	.id = -1,
 };
 static int timer_fd;
@@ -29,6 +34,7 @@ static int create_timer(void)
 		return -1;
 	}
 
+	/* Now we have timer id and we can use it for snd-aloop */
 	printf("Timer id: %d\n", timer.id);
 
 	return 0;
@@ -37,18 +43,24 @@ static int create_timer(void)
 static void load_snd_aloop(void)
 {
 	char command[64];
-	
+
+	/*
+	 * The timer_source would always start with "-1.4." since we are using global timer with
+	 * device number = 4 and subdevice number = <timer_id>
+	 */
 	sprintf(command, "modprobe snd-aloop timer_source=\"-1.4.%d\"", timer.id);
 	system(command);
 }
 
+#define TIMER_FREQ_SEC 1
 static void *ticking_thread(void *data)
 {
 	int i;
 
 	for (i = 0; i < 10; i++) {
+		/* Well, fire the timer! */
 		ioctl(timer_fd, SNDRV_TIMER_IOCTL_FIRE, NULL);
-		sleep(1);
+		sleep(TIMER_FREQ_SEC);
 	}
 }
 
@@ -58,6 +70,7 @@ static void record_play(void)
 {
 	pthread_t tt;
 
+	/* Starting recording and playing at the same time on snd-aloop devices */
 	FILE *rfp = popen("./record.sh", "r");
 	pthread_create(&tt, NULL, ticking_thread, NULL);
 	while (read(fileno(rfp), buf, BUF_SIZE) != 0) {
